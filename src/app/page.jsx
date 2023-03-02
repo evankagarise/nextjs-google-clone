b9e7869cb54e9af2d4b8cc861c3ae9a2c4b2667f
evan <95433637+evankagarise@users.noreply.github.com>
2023-03-02 17:12:15 -0500

    header

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+
+import { Inter } from 'next/font/google'
+
+import HomeHeader from '@/components/HomeHeader'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+    <HomeHeader />
+    </>
+  )
+}
